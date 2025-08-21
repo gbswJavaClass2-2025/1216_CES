d1885bd104af9b68dd7112d9f761e056c9c7766f
choieunsu2009 <jogg86199@gmail.com>
2025-08-21 12:34:08 +0900

    first commit

---
+++ src/sec03/exam01/Hello.java
@@ -0,0 +1,18 @@
+package sec03.exam01;
+
+/**
+ * @author choies
+ */
+
+public class Hello {
+
+    /*
+     * 범위 주석
+     */
+    public static void main(String[] args) {
+        // 콘솔에 출력하기
+        System.out.println("hello, java");
+
+    }
+
+}
