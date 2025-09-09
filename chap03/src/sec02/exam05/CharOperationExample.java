47261a80adaac40018df2fb677e2680d56b68a9b
choieunsu2009 <jogg86199@gmail.com>
2025-09-09 09:25:48 +0900

    0909 chap03

---
+++ chap03/src/sec02/exam05/CharOperationExample.java
@@ -0,0 +1,14 @@
+package sec02.exam05;
+
+public class CharOperationExample {
+    public static void main(String[] args) {
+        char c1 = 'A' + 1;
+        char c2 = 'A';
+//        char c3 = c2 + 1;
+//        char 는 산술 연산에서 사용되면 int 타입으로 변환됨
+        System.out.println("c1: "+c1);
+        System.out.println("c2: "+c2);
+//        System.out.println("c3: "+c3);
+
+    }
+}
